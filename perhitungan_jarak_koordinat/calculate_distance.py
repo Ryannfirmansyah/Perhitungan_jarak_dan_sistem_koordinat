1c2179771f6f0942e2c551f0c6297f557fa61c73
ferastoro <ferarrimeilani@gmail.com>
2024-10-03 11:48:35 +0800

    Menambahkan fungsi 1

--- perhitungan_jarak_koordinat/calculate_distance.py
+++ perhitungan_jarak_koordinat/calculate_distance.py
@@ -1,2 +1,6 @@
- #Fungsi untuk menghitung jarak antar koordinat.
+#Fungsi untuk menghitung jarak antar koordinat.
 #FERA
+
+def hitung_jarak_koordinat(x1, y1, x2, y2):
+    jarak = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5  # akar dari penjumlahan kuadrat selisih koordinat
+    return jarak
