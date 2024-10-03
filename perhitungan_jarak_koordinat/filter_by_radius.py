b15b6218c26d4a787a377e5997aebaa7b072206b
ferastoro <ferarrimeilani@gmail.com>
2024-10-03 11:54:52 +0800

    Menambahkan fungsi 7

--- perhitungan_jarak_koordinat/filter_by_radius.py
+++ perhitungan_jarak_koordinat/filter_by_radius.py
@@ -1,2 +1,13 @@
 #(points, center_point, radius): Fungsi untuk menyaring wilayah berdasarkan radius tertentu.
 #FERA
+
+def saring_wilayah_dalam_radius(koordinat_titik_pusat, daftar_titik, radius):
+    hasil = []
+    x_pusat, y_pusat = koordinat_titik_pusat
+
+    for (x, y) in daftar_titik:
+        jarak = ((x - x_pusat) ** 2 + (y - y_pusat) ** 2) ** 0.5
+        if jarak <= radius:
+            hasil.append((x, y))
+
+    return hasil
