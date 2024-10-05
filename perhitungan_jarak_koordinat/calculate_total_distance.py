1138eff3c6ac22794f35fa51fc45f360283f011d
Sitinurhalisatahir <nurhalisatahirsiti@gmail.com>
2024-10-05 19:41:09 +0800

    Perbaikan Fungsi Pada File calculate_total_distance

--- perhitungan_jarak_koordinat/calculate_total_distance.py
+++ perhitungan_jarak_koordinat/calculate_total_distance.py
@@ -1,24 +1,19 @@
 # (points): Fungsi untuk menghitung jarak total untuk beberapa titik
 # SITI
+# my_package/calculate_total_distance.py
 
-class Titik:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+def jt(titik):
+    total_jarak = 0.0
 
-    def __repr__(self):
-        return f"Titik({self.x}, {self.y})"
-    
-def hitung_jarak_total(titik_titik):
-    total_jarak = 0
-    for i in range(len(titik_titik) - 1):
-        x1, y1 = titik_titik[i].x, titik_titik[i].y
-        x2, y2 = titik_titik[i + 1].x, titik_titik[i + 1].y
+    # Menghitung jarak antara setiap titik berturut-turut
+    for i in range(len(titik) - 1):
+        x1, y1 = titik[i]
+        x2, y2 = titik[i + 1]
+        
+        # Rumus jarak Euclidean yang benar
         jarak = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
         total_jarak += jarak
-    return total_jarak
 
-
+    return total_jarak 
 
     
-
