49b9b96f19848527e16565539f6908a9541ae7da
Jabbar <jabbarfanshurna@gmail.com>
2024-10-07 17:55:32 +0700

    menyederhanakan nama fungsi dan modul derajat ke radian

--- perhitungan_jarak_koordinat/degrees_to_radians.py
+++
@@ -1,5 +0,0 @@
-# (degree): Fungsi untuk mengubah derajat ke radian.
-#JABBAR
-def derajat_ke_radian(derajat):
-    pi = 3.141592653589793
-    return derajat * (pi / 180)
--- perhitungan_jarak_koordinat/km_to_m.py
+++
@@ -1,5 +0,0 @@
-# Fungsi untuk konversi kilometer ke meter.
-#JABBAR
-def konversi_kilometer_ke_meter(kilometer):
-    meter = kilometer * 1000
-    return meter
