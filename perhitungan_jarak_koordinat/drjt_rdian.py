e91120d8bb24835fb95fca1297d36d070c38cb42
Jabbar <jabbarfanshurna@gmail.com>
2024-10-08 17:34:59 +0700

    commit memperbaiki kode

--- perhitungan_jarak_koordinat/drjt_rdian.py
+++
@@ -1,6 +0,0 @@
-# (degree): Fungsi untuk mengubah derajat ke radian.
-#JABBAR
-def drjt_rdn(derajat, desimal=2):
-    pi = 3.141592653589793
-    radian = derajat * (pi / 180)
-    return round(radian, desimal)
--- perhitungan_jarak_koordinat/km_to_m.py
+++ perhitungan_jarak_koordinat/km_to_m.py
@@ -4,15 +4,19 @@ def konv_m(ukuran, jenis="meter"):
     if jenis == "km":
          hasil = ukuran / 1000
     elif jenis == "dm":
-         hasil == ukuran * 10
+         hasil = ukuran * 10
     elif jenis == "cm":
-         hasil == ukuran * 100
+         hasil = ukuran * 100
     elif jenis == "mm":
-         hasil == ukuran * 1000
+         hasil = ukuran * 1000
     elif jenis == "miles":
-         hasil ==  ukuran / 1609.34
+         hasil =  ukuran / 1609.34
     elif jenis == "kaki":
-         hasil == ukuran / 0.3048
+         hasil = ukuran / 0.3048
     elif jenis == "yard":
-         hasil == ukuran / 0.9144
-    return hasil
+         hasil = ukuran / 0.9144
+    return round(hasil, 3)
+
+
+
+
