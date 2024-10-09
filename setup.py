218540f161b3a6e4b9407a4086d5b144df96ed3e
Ryannfirmansyah <ryannnfirmannsyah@gmail.com>
2024-10-09 17:12:36 +0800

    update setup

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="perhitungan_jarak_koordinat",
-    version="0.1.0",
+    version="1.0",
     author="Kelompok 1 Algoritma & Pemrograman B",
-    author_email="",
     description="Package untuk perhitungan jarak dan fungsi sistem koordinat geografis",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Ryannfirmansyah/Perhitungan_jarak_dan_sistem_koordinat.git",  # Ganti dengan URL yang sesuai
     packages=find_packages(),
     install_requires=[
         "geopy",  # library eksternal yang mungkin diperlukan
